b791ca6e4320c2af8b2fb0eee2aaa21fa8a8be4d
KitayDehbi <dehbi.ayoub.98@gmail.com>
2023-08-06 09:57:23 +0100

    fixig tab icon

--- src/component/Models/FruitDetailsModel/FruitDetailsModel.jsx
+++ src/component/Models/FruitDetailsModel/FruitDetailsModel.jsx
@@ -11,20 +11,5 @@ import {
 } from "@chakra-ui/react";
 
 export const FuitDetailsModel = (isOpen, onCloseHandle) => {
-  return <Modal isCentered isOpen={isOpen} onClose={onCloseHandle}>
-  <ModalOverlay />
-  <ModalContent>
-    <ModalHeader>Modal Title</ModalHeader>
-    
-    <ModalBody>
-     <Text>TETS</Text>
-    </ModalBody>
-
-    <ModalFooter>
-      <Button colorScheme='red' mr={3} onClick={onCloseHandle}>
-        Close
-      </Button>
-    </ModalFooter>
-  </ModalContent>
-</Modal> 
+  return 
 };
